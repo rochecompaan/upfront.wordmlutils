86d4668d379d2392c49540cd0e34c8a3ebd3ceef
Roché Compaan <roche@upfrontsystems.co.za>
2011-06-07 17:41:48 +0200

    add image references to document.xml.rels
    use image filename as reference id

--- upfront/wordmlutils/html2wordml.py
+++ upfront/wordmlutils/html2wordml.py
@@ -11,11 +11,12 @@ from PIL import Image
 dirname = os.path.dirname(__file__)
 
 def get_images(doc):
-    images = []
+    images = {}
     for img in doc.xpath('//img'):
         url = img.get('src')
         image = urllib.urlopen(url)
-        images.append((url, StringIO(image.read())))
+        filename = url.split('/')[-1]
+        images[filename] = (url, StringIO(image.read()))
     return images
     
 def convertPixelsToEMU(px):
@@ -36,14 +37,19 @@ def transform(htmlfile, xslfile):
 
     template = zipfile.ZipFile(os.path.join(dirname, 'template.docx'))
 
+    # read and parse relations from template so that we can update it
+    # with links to images
+    rels = template.read('word/_rels/document.xml.rels')
+    rels = etree.parse(StringIO(rels)).getroot()
+
     output = StringIO()
     zf = zipfile.ZipFile(output, 'w')
     namelist = template.namelist()
     docindex = namelist.index('word/document.xml')
-    for count, img in enumerate(images):
+    for filename, img in images.items():
         url, data = img
         # insert image before document
-        namelist.insert(docindex, 'word/media/image%s' % count)
+        namelist.insert(docindex, 'word/media/%s' % filename)
 
         # insert image sizes in the wordml
         img = Image.open(data)
@@ -58,21 +64,27 @@ def transform(htmlfile, xslfile):
         ridattr = '%s-$rid' % url
         wordml = wordml.replace(widthattr, str(width))
         wordml = wordml.replace(heightattr, str(height))
-        wordml = wordml.replace(ridattr, 'rImageId%s' % count)
+        wordml = wordml.replace(ridattr, filename)
+        relxml = """<Relationship Id="%s" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/%s"/>""" % (
+            filename, filename)
+        rels.append(etree.fromstring(relxml))
+
+    relsxml = etree.tostring(rels)
 
     print wordml
 
-    for filename in namelist:
-        if filename == 'word/document.xml':
-            zf.writestr('word/document.xml', wordml)
-        elif filename.startswith('word/media'):
-            index = int(filename[-1])
-            filename = 'word/media/image%s' % index
-            filecontent = images[index][-1].read()
-            zf.writestr(filename, filecontent)
+    for filepath in namelist:
+        if filepath == 'word/document.xml':
+            zf.writestr(filepath, wordml)
+        elif filepath.startswith('word/media'):
+            filename = filepath.split('/')[-1]
+            filecontent = images[filename][-1].read()
+            zf.writestr(filepath, filecontent)
+        elif filepath.startswith('word/_rels/document.xml.rels'):
+            zf.writestr(filepath, relsxml)
         else:
-            content = template.read(filename)
-            zf.writestr(filename, content)
+            content = template.read(filepath)
+            zf.writestr(filepath, content)
         
     template.close()
     zf.close()
